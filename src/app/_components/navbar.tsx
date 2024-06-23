aa4caa06d3954733c4e37e6a1bea078fd7168a4a
Josh Peterson <joshuadanpeterson@gmail.com>
2024-06-22 23:49:29 -0600

    feat(navbar): Built navbar

---
+++ src/app/_components/navbar.tsx
@@ -0,0 +1,92 @@
+// src/app/_components/navbar.tsx
+"use client";
+
+import { FC, useState } from "react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import clsx from "clsx";
+
+const Navbar: FC = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <nav className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+      <div className="flex items-center justify-between w-full md:w-auto">
+        <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
+          josh
+        </h1>
+        <button className="text-white md:hidden" onClick={toggleMenu}>
+          <svg
+            className="w-6 h-6"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d={isOpen ? "M6 18L18 6M6 6l12 12" : "M4 6h16M4 12h16M4 18h16"}
+            ></path>
+          </svg>
+        </button>
+      </div>
+      <ul
+        className={`flex-col md:flex-row flex md:space-x-4 text-lg mt-5 md:pl-8 ${isOpen ? "block" : "hidden"} md:flex`}
+      >
+        <li>
+          <Link
+            href="/"
+            className={clsx(
+              "underline hover:text-blue-600 duration-200 transition-colors",
+              { "text-blue-600": pathname === "/" },
+            )}
+          >
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/portfolio"
+            className={clsx(
+              "underline hover:text-blue-600 duration-200 transition-colors",
+              { "text-blue-600": pathname === "/portfolio" },
+            )}
+          >
+            Portfolio
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/about"
+            className={clsx(
+              "underline hover:text-blue-600 duration-200 transition-colors",
+              { "text-blue-600": pathname === "/about" },
+            )}
+          >
+            About
+          </Link>
+        </li>
+        <li>
+          <Link
+            href="/contact"
+            className={clsx(
+              "underline hover:text-blue-600 duration-200 transition-colors",
+              { "text-blue-600": pathname === "/contact" },
+            )}
+          >
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
