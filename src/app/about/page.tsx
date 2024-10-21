27c302a680280f4657c510e1299af61d02c06d6f
Josh Peterson <joshuadanpeterson@gmail.com>
2024-10-20 21:45:20 -0600

    feat: Update About Me bio

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -7,9 +7,25 @@ const AboutPage = () => {
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
         About Me
       </h1>
-      <p className="text-lg mt-5 md:pl-8 text-center md:text-left max-w-2xl">
-        Welcome to the About page. Here you can learn more about me.
-      </p>
+      <div className="text-lg mt-5 md:pl-8 text-center md:text-left max-w-2xl">
+        <p className="mb-4">
+          I’m <strong>Josh Peterson</strong>, a Special Projects Group Amazon
+          Delivery Dispatcher based in Loveland, CO. With a background in
+          logistics, managing large-scale operations at an Amazon DSP, I ensure
+          the daily coordination of up to 50 drivers and 12,000 packages. Prior
+          to logistics, I spent years in journalism, covering topics from AI to
+          regulatory policies for outlets like The Federalist and the Washington
+          Free Beacon. As a passionate web developer, I’ve completed multiple
+          coding bootcamps, expanding my expertise in Python, JavaScript, and
+          DevOps.
+        </p>
+        <p className="mb-4">
+          Outside of work, I’m a musician, avid hiker, and tech enthusiast
+          interested in AI, space, crypto, and tech policy. My blog dives into
+          these topics, blending my unique background in journalism, logistics,
+          and tech.
+        </p>
+      </div>
     </div>
   );
 };
