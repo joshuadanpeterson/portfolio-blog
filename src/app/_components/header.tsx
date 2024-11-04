7ee3cecd36ff423fda20d71f3777ced94bfc59b1
Josh Peterson <joshuadanpeterson@gmail.com>
2024-11-03 19:36:37 -0700

    feat(header): Set "Blog." text to black and add blue hover effect
    - Ensured the "Blog." text is always black using `!text-black`
    - Added `hover:!text-blue-500` to change text color to blue on hover

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,12 +1,16 @@
+// src/app/_components/header.tsx
+
 import Link from "next/link";
 
 const Header = () => {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
+      <Link
+        href="/posts"
+        className="hover:underline !text-black hover:!text-blue-500"
+      >
+        Blog.
       </Link>
-      .
     </h2>
   );
 };
