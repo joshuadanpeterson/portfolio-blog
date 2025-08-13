b5f9f9548eab6cfe054273a0d19457056b50b2a2
Josh Peterson <joshuadanpeterson@gmail.com>
2025-08-12 21:02:55 -0700

    fix(theme): ☀️ make theme toggle icons white in dark mode
    - Apply dark:text-white to sun/moon svgs via className
    - Keep button text color and border suitable in dark mode

--- src/app/_components/ThemeToggle.tsx
+++ src/app/_components/ThemeToggle.tsx
@@ -15,28 +15,30 @@ export default function ThemeToggle() {
     >
       {isDark ? (
         // Sun icon when currently dark (action: go light)
-        <svg
+        svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           width="18"
           height="18"
           fill="currentColor"
+          className="text-black dark:text-white"
           aria-hidden="true"
           focusable="false"
-        >
+        
           <path d="M12 18a6 6 0 1 0 0-12 6 6 0 0 0 0 12Zm0 4a1 1 0 0 1-1-1v-1a1 1 0 1 1 2 0v1a1 1 0 0 1-1 1ZM12 4a1 1 0 0 1-1-1V2a1 1 0 1 1 2 0v1a1 1 0 0 1-1 1Zm8 8a1 1 0 0 1 1-1h1a1 1 0 1 1 0 2h-1a1 1 0 0 1-1-1ZM2 12a1 1 0 0 1 1-1H4a1 1 0 1 1 0 2H3a1 1 0 0 1-1-1Zm14.95 6.364a1 1 0 0 1 1.415 0l.707.707a1 1 0 1 1-1.414 1.415l-.708-.707a1 1 0 0 1 0-1.415ZM4.635 4.636a1 1 0 0 1 1.415 0l.707.707A1 1 0 1 1 5.343 6.758l-.707-.707a1 1 0 0 1 0-1.415Zm12.728-1e-3a1 1 0 0 1 0 1.415l-.707.707A1 1 0 1 1 15.242 5.05l.707-.707a1 1 0 0 1 1.414 0ZM6.05 18.364a1 1 0 0 1 0 1.415l-.707.707A1 1 0 1 1 3.93 19.07l.707-.707a1 1 0 0 1 1.414 0Z" />
         </svg>
       ) : (
         // Moon icon when currently light (action: go dark)
-        <svg
+        svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 24 24"
           width="18"
           height="18"
           fill="currentColor"
+          className="text-black dark:text-white"
           aria-hidden="true"
           focusable="false"
-        >
+        
           <path d="M21.752 15.002A9 9 0 0 1 9 2.248 9.002 9.002 0 1 0 21.752 15.002Zm-2.104 1.695A7 7 0 1 1 11.303 3.35a9 9 0 0 0 8.345 13.347Z" />
         </svg>
       )}
